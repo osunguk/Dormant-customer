83c5a5d889090fffcd7ff054a6ace043918e2199
osunguk <osunguk@gmail.com>
2019-08-13 15:24:31 +0900

    http://blog.weirdx.io/post/39953 참고해서 1:1 대응 데이터 모델 생

--- dormant_account/app/admin.py
+++ dormant_account/app/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
-from .models import Content,Profile
+from .models import Content, Profile
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'profile'
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (ProfileInline, )
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
+
 admin.site.register(Content)
 admin.site.register(Profile)
+
--- dormant_account/app/models.py
+++ dormant_account/app/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 
@@ -20,3 +22,15 @@ class Content(models.Model):
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     dormant_count = models.DateField(null=True, blank=True)
+    nickname = models.CharField('nickname', max_length=100, blank=True)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
