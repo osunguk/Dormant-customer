cffdadc151dc9efc7d626148e1d38d3fcc242a35
osunguk <osunguk@gmail.com>
2019-08-14 13:55:47 +0900

    view에 user_list 에서 사용자의 휴면계정 전환 남은 기간 표시는 html 완성

--- dormant_account/app/views.py
+++ dormant_account/app/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from .models import Content, Profile
-from django.contrib.auth.models import User, Group,AnonymousUser
+from django.contrib.auth.models import User, Group, AnonymousUser
 from django.contrib import auth
 
 from django.utils import timezone
@@ -9,26 +9,32 @@ from django.db.models import Max
 import datetime
 from apscheduler.schedulers.background import BackgroundScheduler
 
-#휴면계정 알림
+
+# 휴면계정 알림
 def dormant_Alert():
     userList = User.objects.values()
 
     for users in userList:
-        last_login = users['last_login']
         now = datetime.datetime.now(timezone.utc)
+        last_login = users['last_login']
         if last_login is None:
             last_login = users['date_joined']
-        if now == (last_login + datetime.timedelta(days=335)): # 휴면계정 변환 30일 전 알림
-            print('ID : '+users['username'] + '은(는) 30일 뒤 휴면계정으로 전환됩니다.')
-    #print('Background scheduler \'dormant_Alter()\' start')
+        dormant_Time = last_login + datetime.timedelta(days=335) - now
+        Profile.objects.filter(id=users['id']).update(dormant_cnt=dormant_Time.days)
+        if last_login is None:
+            last_login = users['date_joined']
+        if now == (last_login + datetime.timedelta(days=335)):  # 휴면계정 변환 30일 전 알림
+            print('ID : ' + users['username'] + '은(는) 30일 뒤 휴면계정으로 전환됩니다.')
+    # print('Background scheduler \'dormant_Alter()\' start')
 
-#휴면계정 전환
+
+# 휴면계정 전환
 def change_AccountGroup():
     userList = User.objects.values()
     dormant_group = Group.objects.get(name='dormant_account')
     general_group = Group.objects.get(name='General users')
     for users in userList:
-        user = User.objects.get(username=users['username']) #유저리스트에서 username 가져옴
+        user = User.objects.get(username=users['username'])  # 유저리스트에서 username 가져옴
         last_login = users['last_login']
 
         now = datetime.datetime.now(timezone.utc)
@@ -39,13 +45,8 @@ def change_AccountGroup():
         if (now - last_login).days >= 365:
             dormant_group.user_set.add(user)
             user.groups.remove(general_group)
-            print('ID : '+users['username'] + '은(는) 휴면계정으로 전환되었습니다.')
+            print('ID : ' + users['username'] + '은(는) 휴면계정으로 전환되었습니다.')
 
-            """
-            tempgroup = User.groups.through.objects.get(user=users)
-            tempgroup.group = dormant_group
-            tempgroup.save()
-            """
 
 sched = BackgroundScheduler()
 sched.add_job(change_AccountGroup, 'interval', seconds=60)
@@ -53,32 +54,33 @@ sched.add_job(dormant_Alert, 'interval', seconds=3)
 sched.start()
 
 
-
 def home(request):
-    return render(request,'app/home.html')
+    return render(request, 'app/home.html')
 
 
 def login(request):
     if request.method == 'POST':
         name = request.POST.get('username')
         pwd = request.POST.get('pwd')
-        user = auth.authenticate(request, username=name, password=pwd) #인증
+        user = auth.authenticate(request, username=name, password=pwd)  # 인증
         general_group = Group.objects.get(name='General users')
         check_DormantAccount = user.groups.filter(name='dormant_account').exists()
         content_all = Content.objects.all()
         total_content = len(content_all)  # 총 게시물 수
         if user is not None:
             auth.login(request, user)
-            #Profile(dormant_count = 0).save()  # 로그인 했을 때 휴면 계정 전환 카운트 초기화
+            # Profile(dormant_count = 0).save()  # 로그인 했을 때 휴면 계정 전환 카운트 초기화
             if user.groups.filter(name='dormant_account').exists():  # 휴면계정일때 로그인 하면 일반그룹으로 이동
                 tempgroup = User.groups.through.objects.get(user=user)  # 임시그룹
                 tempgroup.group = general_group
                 tempgroup.save()
-            return render(request, 'app/board.html',{'check_DormantAccount':check_DormantAccount,'contents': content_all,'total':total_content})
+            return render(request, 'app/board.html',
+                          {'check_DormantAccount': check_DormantAccount, 'contents': content_all,
+                           'total': total_content})
         else:
-            return render(request, 'app/login.html',{'error':'잘못된 id 또는 pwd 입니다'})
+            return render(request, 'app/login.html', {'error': '잘못된 id 또는 pwd 입니다'})
     else:
-        return render(request,'app/login.html')
+        return render(request, 'app/login.html')
 
 
 def logout(request):
@@ -90,7 +92,7 @@ def signup(request):
     if request.method == 'POST':
         username = request.POST['name']
         userpwd = request.POST['pwd']
-        user = User.objects.create_user(username=username,password=userpwd)
+        user = User.objects.create_user(username=username, password=userpwd)
 
         # 회원가입 후 일반유저 그룹에 추가
         general_group = Group.objects.get(name='General users')
@@ -108,7 +110,8 @@ def write(request):
         title = request.POST['title']
         content = request.POST.get('content')
         Content.user = request.user
-        Content(number=number.get('number')+1, title=title, contents=content, writer=User.get_username(Content.user)).save()
+        Content(number=number.get('number') + 1, title=title, contents=content,
+                writer=User.get_username(Content.user)).save()
         return redirect(to='board')
 
     return render(request, 'app/write.html')
@@ -116,11 +119,13 @@ def write(request):
 
 def board(request):
     content_all = Content.objects.all()
-    total_content = len(content_all)# 총 게시물 수
+    total_content = len(content_all)  # 총 게시물 수
     dormant_account = None
     user = request.user
 
-    return render(request, 'app/board.html', {'contents': content_all,'dormant_account':dormant_account,'userdata':user,'total':total_content})
+    return render(request, 'app/board.html',
+                  {'contents': content_all, 'dormant_account': dormant_account, 'userdata': user,
+                   'total': total_content})
 
 
 def user(request):
@@ -132,16 +137,18 @@ def user(request):
     day = (now - last_login).days
     result = (now - last_login)
 
-    return render(request, 'app/user.html',{'name':user,'last_login':last_login,'joined_data':joined_data,'now':now,'result':result,'day':day})
+    return render(request, 'app/user.html',
+                  {'name': user, 'last_login': last_login, 'joined_data': joined_data, 'now': now, 'result': result,
+                   'day': day})
 
 
-def detail(request,number):  # 해당 number의 게시물을 불러와 html로 전송
-    content = get_object_or_404(Content, number = number)
+def detail(request, number):  # 해당 number의 게시물을 불러와 html로 전송
+    content = get_object_or_404(Content, number=number)
 
-    return render(request,'app/detail.html',{'content':content})
+    return render(request, 'app/detail.html', {'content': content})
 
 
-def delete(request,number):
+def delete(request, number):
     content = Content.objects.get(number=number)
     if content.writer == request.user.get_username():  # 작성자와 사용자를 비교 일치시만 수정가능
         content.delete()
@@ -151,23 +158,27 @@ def delete(request,number):
         return render(request, 'app/detail.html', {'content': content, 'alert': alert})
 
 
-def edit(request,number):
+def edit(request, number):
     content = Content.objects.get(number=number)
     if request.method == 'POST':  # 수정된 내용을 입력한후 detail로 이동
         content.title = request.POST['title']
         content.contents = request.POST['content']
         content.last_edit = datetime.datetime.now(timezone.utc)
         content.save()
-        return render(request,'app/detail.html',{'content':content})
+        return render(request, 'app/detail.html', {'content': content})
     else:  # 수정 페이지로 이동
         if content.writer == request.user.get_username():  # 작성자와 사용자를 비교 일치시만 수정가능
-            return render(request,'app/edit.html',{'content':content})
+            return render(request, 'app/edit.html', {'content': content})
         else:
             alert = True
-            return render(request,'app/detail.html',{'content':content,'alert':alert})
+            return render(request, 'app/detail.html', {'content': content, 'alert': alert})
 
 
 def user_list(request):
-    userList = User.objects.values()
-    return render(request,'app/user_list.html',{'userList':userList})
-
+    pList = Profile.objects.values()
+    result = []
+    for x in list(pList):
+        u = User.objects.get(id=x['user_id'])
+        sentence = u.username + '의 휴면계정 전환까지 남은 기간 : ' + str(x['dormant_cnt']) + '일'
+        result.append(sentence)
+    return render(request, 'app/user_list.html', {'results':result})
