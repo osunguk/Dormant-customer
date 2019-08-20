a0fcb7aac385188ccc3cc54144139e8d32a5c533
osunguk <osunguk@gmail.com>
2019-08-20 14:18:31 +0900

    Merge branch 'master' into HEAD
    # Conflicts:
    #	dormant_account/app/__pycache__/admin.cpython-37.pyc
    #	dormant_account/app/__pycache__/models.cpython-37.pyc
    #	dormant_account/app/__pycache__/views.cpython-37.pyc
    #	dormant_account/app/admin.py
    #	dormant_account/app/migrations/0001_initial.py
    #	dormant_account/app/migrations/__pycache__/0001_initial.cpython-37.pyc
    #	dormant_account/app/migrations/__pycache__/0012_profile.cpython-37.pyc
    #	dormant_account/app/models.py
    #	dormant_account/dormant_account/__pycache__/__init__.cpython-37.pyc
    #	dormant_account/dormant_account/__pycache__/settings.cpython-37.pyc
    #	dormant_account/dormant_account/__pycache__/urls.cpython-37.pyc
    #	dormant_account/dormant_account/settings.py
    #	venvproject/myvenv/Lib/site-packages/MySQLdb/__pycache__/__init__.cpython-37.pyc
    #	venvproject/myvenv/Lib/site-packages/MySQLdb/__pycache__/_exceptions.cpython-37.pyc
    #	venvproject/myvenv/Lib/site-packages/MySQLdb/__pycache__/compat.cpython-37.pyc
    #	venvproject/myvenv/Lib/site-packages/MySQLdb/__pycache__/connections.cpython-37.pyc
    #	venvproject/myvenv/Lib/site-packages/MySQLdb/__pycache__/converters.cpython-37.pyc
    #	venvproject/myvenv/Lib/site-packages/MySQLdb/__pycache__/cursors.cpython-37.pyc
    #	venvproject/myvenv/Lib/site-packages/MySQLdb/__pycache__/release.cpython-37.pyc
    #	venvproject/myvenv/Lib/site-packages/MySQLdb/__pycache__/times.cpython-37.pyc
    #	venvproject/myvenv/Lib/site-packages/MySQLdb/constants/__pycache__/CLIENT.cpython-37.pyc
    #	venvproject/myvenv/Lib/site-packages/MySQLdb/constants/__pycache__/FIELD_TYPE.cpython-37.pyc
    #	venvproject/myvenv/Lib/site-packages/MySQLdb/constants/__pycache__/FLAG.cpython-37.pyc
    #	venvproject/myvenv/Lib/site-packages/MySQLdb/constants/__pycache__/__init__.cpython-37.pyc

--- dormant_account/app/models.py
+++ dormant_account/app/models.py
@@ -4,42 +4,6 @@ from django.contrib.auth.models import User, PermissionsMixin
 
 from django.db.models.signals import post_save  # 장고의 signals로 특정 이벤트가 발생했을 때 신호를 통한 여러 작업들을 도와주는 모듈
 from django.dispatch import receiver  # 위와 동문
-from django.contrib.auth.models import (
-    BaseUserManager, AbstractBaseUser
-)
-
-
-class MyUserManager(BaseUserManager):
-    def create_user(self, email, date_of_birth, password=None):
-        """
-        Creates and saves a User with the given email, date of
-        birth and password.
-        """
-        if not email:
-            raise ValueError('Users must have an email address')
-
-        user = self.model(
-            email=self.normalize_email(email),
-            date_of_birth=date_of_birth,
-        )
-
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, email, date_of_birth, password):
-        """
-        Creates and saves a superuser with the given email, date of
-        birth and password.
-        """
-        user = self.create_user(
-            email,
-            password=password,
-            date_of_birth=date_of_birth,
-        )
-        user.is_admin = True
-        user.save(using=self._db)
-        return user
 
 
 class Content(models.Model):
@@ -62,34 +26,41 @@ class Profile(models.Model):
         (BUSINESS, 'Business'),
         (CUSTOMER, 'Customer')
     )
+    # 기본 정보
+    email = models.CharField('이메일', max_length=100, blank=True)
+    phoneNumber = models.CharField('핸드폰 번호', max_length=11, blank=True, null=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)  # 기존 User 모델에 1:1 대응을 생성
     check = models.CharField('check', max_length=100, blank=True)
     dormant_cnt = models.IntegerField('dormant_cnt', default=0)
     memo = models.TextField('memo', max_length=300)
     check_alert = models.BooleanField('check_alter', default=False, blank=True)
+    role_profile = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, null=True, blank=True)
 
     def __str__(self):
         return str(self.user.username)
 
-"""
-class DormantUserC(models.Model):
-    email = models.CharField('이메일', max_length=100)
-    kakaoId = models.CharField('카카오톡 아이디', max_length=100)
-    phoneNumber = models.IntegerField('핸드폰 번호', max_length=15)
-    name = models.CharField('ID', max_length=100)
-    point = models.IntegerField('보유 포인트', max_length=100000)
+
+class UserC(models.Model):
+    user_c = models.ForeignKey(User, on_delete=models.CASCADE)
+    kakao_Id = models.CharField('카카오톡 아이디', max_length=100)
+    mining_point = models.IntegerField('보유 포인트',)
 
 
-class DormantUserB(models.Model):
-    email = models.CharField('이메일', max_length=100)
-    businessNumber = models.IntegerField('사업자 번호', max_length=11)
-    phoneNumber = models.IntegerField('핸드폰 번호', max_length=15)
-    name = models.CharField('ID', max_length=100)
-    point = models.IntegerField('보유 포인트', max_length=100000)
-"""
+class UserB(models.Model):
+    user_b = models.ForeignKey(User, on_delete=models.CASCADE)
+    company_name = models.CharField('사업장 이름', max_length=100)
+    business_number = models.IntegerField('사업자 번호',)
+    star_point = models.IntegerField('보유 포인트',)
 
 
 class DormantUserInfo(models.Model):
+    BUSINESS = 1
+    CUSTOMER = 2
+    ROLE_CHOICES = (
+        (BUSINESS, 'Business'),
+        (CUSTOMER, 'Customer')
+    )
+    role_dormant = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, null=True, blank=True)
     lastLogin = models.DateTimeField(blank=True)
     dormantDate = models.DateTimeField(default=timezone.now)
     deleteDate = models.DateTimeField(blank=True)
